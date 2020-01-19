562ff869ba720d63c0dfa3088272fd4b8228fec8
Michael Rhodes <mrhodes@mediapolis.com>
2020-01-19 01:36:30 -0400

    .

---
+++ modularize/lang.default.js
@@ -0,0 +1,11 @@
+import isArray from './isArray.js';
+import isObject from './isObject.js';
+import isObjectLike from './isObjectLike.js';
+import isSymbol from './isSymbol.js';
+import toNumber from './toNumber.js';
+import toString from './toString.js';
+
+export default {
+  isArray, isObject, isObjectLike, isSymbol, toNumber,
+  toString
+};
