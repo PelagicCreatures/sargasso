ef8ce538da7cce6247af203641a1250c94b09aca
Michael Rhodes <mrhodes@mediapolis.com>
2021-03-09 00:03:12 -0400

    switch from js-cookie to es-cookie

--- dist/sargasso.iife.js
+++ dist/sargasso.iife.js
@@ -4894,172 +4894,97 @@ var SargassoModule = (function (exports) {
 
 	registerSargassoClass('SargassoSupervisor', SargassoSupervisor);
 
-	/*!
-	 * JavaScript Cookie v2.2.1
-	 * https://github.com/js-cookie/js-cookie
-	 *
-	 * Copyright 2006, 2015 Klaus Hartl & Fagner Brack
-	 * Released under the MIT license
-	 */
+	"use strict";
+	var __assign = (commonjsGlobal && commonjsGlobal.__assign) || function () {
+	    __assign = Object.assign || function(t) {
+	        for (var s, i = 1, n = arguments.length; i < n; i++) {
+	            s = arguments[i];
+	            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+	                t[p] = s[p];
+	        }
+	        return t;
+	    };
+	    return __assign.apply(this, arguments);
+	};
 
-	var js_cookie = createCommonjsModule(function (module, exports) {
-	;(function (factory) {
-		var registeredInModuleLoader;
-		if (typeof undefined === 'function' && undefined.amd) {
-			undefined(factory);
-			registeredInModuleLoader = true;
-		}
-		if ('object' === 'object') {
-			module.exports = factory();
-			registeredInModuleLoader = true;
-		}
-		if (!registeredInModuleLoader) {
-			var OldCookies = window.Cookies;
-			var api = window.Cookies = factory();
-			api.noConflict = function () {
-				window.Cookies = OldCookies;
-				return api;
-			};
-		}
-	}(function () {
-		function extend () {
-			var i = 0;
-			var result = {};
-			for (; i < arguments.length; i++) {
-				var attributes = arguments[ i ];
-				for (var key in attributes) {
-					result[key] = attributes[key];
-				}
-			}
-			return result;
-		}
+	function stringifyAttribute(name, value) {
+	    if (!value) {
+	        return '';
+	    }
+	    var stringified = '; ' + name;
+	    if (value === true) {
+	        return stringified; // boolean attributes shouldn't have a value
+	    }
+	    return stringified + '=' + value;
+	}
+	function stringifyAttributes(attributes) {
+	    if (typeof attributes.expires === 'number') {
+	        var expires = new Date();
+	        expires.setMilliseconds(expires.getMilliseconds() + attributes.expires * 864e+5);
+	        attributes.expires = expires;
+	    }
+	    return stringifyAttribute('Expires', attributes.expires ? attributes.expires.toUTCString() : '')
+	        + stringifyAttribute('Domain', attributes.domain)
+	        + stringifyAttribute('Path', attributes.path)
+	        + stringifyAttribute('Secure', attributes.secure)
+	        + stringifyAttribute('SameSite', attributes.sameSite);
+	}
+	function encode(name, value, attributes) {
+	    return encodeURIComponent(name)
+	        .replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent) // allowed special characters
+	        .replace(/\(/g, '%28').replace(/\)/g, '%29') // replace opening and closing parens
+	        + '=' + encodeURIComponent(value)
+	        // allowed special characters
+	        .replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent)
+	        + stringifyAttributes(attributes);
+	}
+	var encode_1 = encode;
+	function parse(cookieString) {
+	    var result = {};
+	    var cookies = cookieString ? cookieString.split('; ') : [];
+	    var rdecode = /(%[\dA-F]{2})+/gi;
+	    for (var i = 0; i < cookies.length; i++) {
+	        var parts = cookies[i].split('=');
+	        var cookie = parts.slice(1).join('=');
+	        if (cookie.charAt(0) === '"') {
+	            cookie = cookie.slice(1, -1);
+	        }
+	        try {
+	            var name_1 = parts[0].replace(rdecode, decodeURIComponent);
+	            result[name_1] = cookie.replace(rdecode, decodeURIComponent);
+	        }
+	        catch (e) {
+	            // ignore cookies with invalid name/value encoding
+	        }
+	    }
+	    return result;
+	}
+	var parse_1 = parse;
+	function getAll() {
+	    return parse(document.cookie);
+	}
+	var getAll_1 = getAll;
+	function get(name) {
+	    return getAll()[name];
+	}
+	var get_1 = get;
+	function set(name, value, attributes) {
+	    document.cookie = encode(name, value, __assign({ path: '/' }, attributes));
+	}
+	var set_1 = set;
+	function remove(name, attributes) {
+	    set(name, '', __assign(__assign({}, attributes), { expires: -1 }));
+	}
+	var remove_1 = remove;
 
-		function decode (s) {
-			return s.replace(/(%[0-9A-Z]{2})+/g, decodeURIComponent);
-		}
-
-		function init (converter) {
-			function api() {}
-
-			function set (key, value, attributes) {
-				if (typeof document === 'undefined') {
-					return;
-				}
-
-				attributes = extend({
-					path: '/'
-				}, api.defaults, attributes);
-
-				if (typeof attributes.expires === 'number') {
-					attributes.expires = new Date(new Date() * 1 + attributes.expires * 864e+5);
-				}
-
-				// We're using "expires" because "max-age" is not supported by IE
-				attributes.expires = attributes.expires ? attributes.expires.toUTCString() : '';
-
-				try {
-					var result = JSON.stringify(value);
-					if (/^[\{\[]/.test(result)) {
-						value = result;
-					}
-				} catch (e) {}
-
-				value = converter.write ?
-					converter.write(value, key) :
-					encodeURIComponent(String(value))
-						.replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
-
-				key = encodeURIComponent(String(key))
-					.replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent)
-					.replace(/[\(\)]/g, escape);
-
-				var stringifiedAttributes = '';
-				for (var attributeName in attributes) {
-					if (!attributes[attributeName]) {
-						continue;
-					}
-					stringifiedAttributes += '; ' + attributeName;
-					if (attributes[attributeName] === true) {
-						continue;
-					}
-
-					// Considers RFC 6265 section 5.2:
-					// ...
-					// 3.  If the remaining unparsed-attributes contains a %x3B (";")
-					//     character:
-					// Consume the characters of the unparsed-attributes up to,
-					// not including, the first %x3B (";") character.
-					// ...
-					stringifiedAttributes += '=' + attributes[attributeName].split(';')[0];
-				}
-
-				return (document.cookie = key + '=' + value + stringifiedAttributes);
-			}
-
-			function get (key, json) {
-				if (typeof document === 'undefined') {
-					return;
-				}
-
-				var jar = {};
-				// To prevent the for loop in the first place assign an empty array
-				// in case there are no cookies at all.
-				var cookies = document.cookie ? document.cookie.split('; ') : [];
-				var i = 0;
-
-				for (; i < cookies.length; i++) {
-					var parts = cookies[i].split('=');
-					var cookie = parts.slice(1).join('=');
-
-					if (!json && cookie.charAt(0) === '"') {
-						cookie = cookie.slice(1, -1);
-					}
-
-					try {
-						var name = decode(parts[0]);
-						cookie = (converter.read || converter)(cookie, name) ||
-							decode(cookie);
-
-						if (json) {
-							try {
-								cookie = JSON.parse(cookie);
-							} catch (e) {}
-						}
-
-						jar[name] = cookie;
-
-						if (key === name) {
-							break;
-						}
-					} catch (e) {}
-				}
-
-				return key ? jar[key] : jar;
-			}
-
-			api.set = set;
-			api.get = function (key) {
-				return get(key, false /* read as raw */);
-			};
-			api.getJSON = function (key) {
-				return get(key, true /* read as json */);
-			};
-			api.remove = function (key, attributes) {
-				set(key, '', extend(attributes, {
-					expires: -1
-				}));
-			};
-
-			api.defaults = {};
-
-			api.withConverter = init;
-
-			return api;
-		}
-
-		return init(function () {});
-	}));
-	});
+	var esCookie = /*#__PURE__*/Object.defineProperty({
+		encode: encode_1,
+		parse: parse_1,
+		getAll: getAll_1,
+		get: get_1,
+		set: set_1,
+		remove: remove_1
+	}, '__esModule', {value: true});
 
 	/**
 		Breakpoints
@@ -5234,7 +5159,7 @@ var SargassoModule = (function (exports) {
 		};
 
 		getCookie (key) {
-			return js_cookie.get(key)
+			return esCookie.get(key)
 		};
 
 		setCookie (key, value, expires) {
@@ -5243,7 +5168,7 @@ var SargassoModule = (function (exports) {
 				domain: this.options.cookieDomain || null,
 				expires: expires
 			};
-			js_cookie.set(key, value, options);
+			esCookie.set(key, value, options);
 		};
 
 		deleteCookie (key) {
