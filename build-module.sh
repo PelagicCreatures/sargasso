97d031e46898bca4d7212faf739c4a21360279fc
Michael Rhodes <mrhodes@mediapolis.com>
2023-04-30 15:16:55 -0400

    latest es-module-shims
    move SargassoComponent to /dist/Component.mjs

--- build-module.sh
+++ build-module.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 npx parcel build index.mjs;
+cp lib/breakpoints.js dist/breakpoints.js;
+cp lib/SargassoComponent.mjs dist/Component.mjs;
 cp lib/modules/MaterialButton.mjs dist/modules/MaterialButton.mjs;
 cp lib/modules/MaterialTextField.mjs dist/modules/MaterialTextField.mjs;
 cp lib/MDC/MDCRipple.mjs dist/modules/MDCRipple.mjs;
 cp lib/MDC/MDCTextField.mjs dist/modules/MDCTextField.mjs;
-cp lib/breakpoints.js dist/breakpoints.js;
-cp lib/SargassoComponent.mjs dist/component/index.mjs;
 npx terser dist/sargasso.mjs --output dist/sargasso.min.mjs;
 npx terser dist/breakpoints.js --output dist/breakpoints.min.js
